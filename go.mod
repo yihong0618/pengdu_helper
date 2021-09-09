31be057f6ee4f6d3e679b9cd26461c1d6f69a470
yihong0618 <zouzou0208@gmail.com>
2021-09-09 17:11:22 +0800

    feat: init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yihong0618/pengdu_helper
+
+go 1.14
+
+require github.com/bmaupin/go-epub v0.5.3
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/bmaupin/go-epub"
+)
+
+var BASE_LRC_URL = "https://music.163.com/api/song/lyric/?id="
+var BASE_MUSIC_URL = "https://y.music.163.com/m/song?id="
+
+var (
+	telegramID    int64
+	telegramToken string
+	toParseString string
+)
+
+func init() {
+	flag.StringVar(&toParseString, "nestring", "", "")
+	flag.Int64Var(&telegramID, "tgid", 0, "telegram room id")
+	flag.StringVar(&telegramToken, "tgtoken", "", "token from telegram")
+}
+
+func parseTitle(toParseString string) string {
+	m := regexp.MustCompile(`《(.*)》`)
+	if m.MatchString(toParseString) {
+		return m.FindStringSubmatch(toParseString)[1]
+	}
+	return "Not Found Title"
+}
+
+func parseId(toParseString string) string {
+	m := regexp.MustCompile((`\?id=(\d+)`))
+	if m.MatchString(toParseString) {
+		return m.FindStringSubmatch(toParseString)[1]
+	}
+	return "Not Found ID"
+}
+
+func getLRCByID(id string) string {
+	lrc_url := BASE_LRC_URL + id + "&lv=-1"
+	resp, err := http.Get(lrc_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var generic map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&generic)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lrc := generic["lrc"].(map[string]interface{})
+	return lrc["lyric"].(string)
+}
+
+func main() {
+	flag.Parse()
+	title := parseTitle(toParseString)
+	musicId := parseId(toParseString)
+	content := getLRCByID(musicId)
+	m := regexp.MustCompile(`(\[(.*?)\])`)
+	content = m.ReplaceAllString(content, "")
+	contentList := strings.Split(content, "\n")
+	e := epub.NewEpub(title)
+
+	e.SetAuthor("test")
+	body := "<h1>" + title + "</h1>"
+	link := BASE_MUSIC_URL + musicId
+	body += "<a href=\"" + link + "\">" + link + "</a>"
+	for i := 0; i < len(contentList); i++ {
+		if len(contentList[i]) > 0 {
+			body = body + ("<p>" + contentList[i] + "</p>")
+		}
+	}
+	// Add the section
+	e.AddSection(body, "", "", "")
+	err := e.Write("music_lrc.epub")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
